b7ffa6f33d3e2948a860d7aa54bbf9cd426df5d3
= <=>
2025-11-29 12:39:05 -0300

    Adicionando exeplo e anotações de estudo

--- alula-2-modo-interativo.py
+++ alula-2-modo-interativo.py
@@ -1,5 +1,17 @@
 """
 O interpretador Python pode executar em modo que possibilite o desenvolvedor a escrever código, e ver o resultado na hora.
 
+Esse modo é chamado de modo interativo, e pode ser acessado através do terminal ou prompt de comando, digitando o comando "python" ou "python3", dependendo da instalação.
+
+Exemplo:
+
+$ python3
+>>>
+>>> print("Olá, Mundo!")
+Olá, Mundo!
+>>>
+>>> 2 + 2
+4
+
 """
 
