f18dfe4df61a04cc492dad43df15ef36a71b6a13
= <=>
2025-11-29 12:50:15 -0300

    Funções dir e help

--- alula-2-modo-interativo.py
+++ alula-2-modo-interativo.py
@@ -14,4 +14,39 @@ Olá, Mundo!
 4
 
 """
+# Função dir() . Mostra os atributos e métodos disponíveis para um determinado tipo ou objeto.
+print(dir(int))
+print("-----------------------------------------------------------------------------------------------")
+print() 
+print(dir(str))
+print("-----------------------------------------------------------------------------------------------")
+print()
+print(dir(float))
+print("-----------------------------------------------------------------------------------------------")
+print()
+print(dir(list))
+print("-----------------------------------------------------------------------------------------------")
+print()
+print(dir(dict))
 
+print("-----------------------------------------------------------------------------------------------")
+print()
+# Função help() . Mostra a documentação de um determinado tipo ou objeto.
+print(help(int))
+print(help(str))
+print(help(float))
+print(help(list))
+print(help(dict))
+
+print("-----------------------------------------------------------------------------------------------")
+print()
+# Podemos criar nossos próprios tipos (classes) e ver seus atributos e métodos.
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e tenho {self.idade} anos.")
+pessoa = Pessoa("Ana", 30)
+print(dir(pessoa))
+print(help(pessoa))
