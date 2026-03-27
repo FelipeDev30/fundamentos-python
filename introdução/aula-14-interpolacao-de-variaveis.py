a61cf42036546af78a94c3869cb7baa9e19716e9
= <=>
2026-03-26 21:22:21 -0300

    Melhorando o estudo sobre interpolação de strings

--- introdução/aula-14-interpolacao-de-variaveis.py
+++ introdução/aula-14-interpolacao-de-variaveis.py
@@ -8,15 +8,24 @@ Existem várias maneiras de fazer isso, incluindo o uso do operador `%`, o méto
 
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
+profissao = input("Digite sua profissão: ")
+linguagem = input("Digite sua linguagem de programação favorita: ")
+
+dados = {
+    "nome": nome,
+    "idade": idade,
+    "profissao": profissao,
+    "linguagem": linguagem
+}
+
 # Usando o operador %%
-mensagem1 = "Olá, %s! Você tem %d anos." % (nome, idade)
+mensagem1 = "Olá, %s! Você tem %d anos, trabalha como %s e gosta de programar em %s." % (dados["nome"], dados["idade"], dados["profissao"], dados["linguagem"])
 print(mensagem1)
 
 # Usando o método str.format()
-mensagem2 = "Olá, {}! Você tem {} anos.".format(nome, idade)
+mensagem2 = "Olá, {}! Você tem {} anos, trabalha como {} e gosta de programar em {}.".format(dados["nome"], dados["idade"], dados["profissao"], dados["linguagem"])
 print(mensagem2)
 
 # Usando f-strings
-mensagem3 = f"Olá, {nome}! Você tem {idade} anos."
-print(mensagem3)
-
+mensagem3 = f"Olá, {dados['nome']}! Você tem {dados['idade']} anos, trabalha como {dados['profissao']} e gosta de programar em {dados['linguagem']}."
+print(mensagem3) 
