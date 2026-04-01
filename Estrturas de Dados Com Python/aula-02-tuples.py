035bf6d92cd7fc6954c7f99ce41e864d696d7380
= <=>
2026-04-01 19:47:21 -0300

    Att

---
+++ Estrturas de Dados Com Python/aula-02-tuples.py
@@ -0,0 +1,14 @@
+""" 
+# Criando tuplas
+
+Tuplas são estruturas de dados imutáveis, ou seja, uma vez criadas, seus elementos não podem ser alterados. 
+Elas são definidas usando parênteses () e os elementos são separados por vírgulas.
+
+"""
+# Criando uma tupla
+minha_tupla = (1, 2, 3, "Python", True)
+print("Minha tupla:", minha_tupla)
+
+# Acessando elementos da tupla
+print("Primeiro elemento:", minha_tupla[0])  # Acessa o primeiro elemento
+print("Último elemento:", minha_tupla[-1])  # Acessa o último elemento
