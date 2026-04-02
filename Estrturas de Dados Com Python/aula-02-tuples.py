e389fc5f9416410ac7d955a88e7acffd0c43697f
= <=>
2026-04-01 22:25:21 -0300

    Aprimorando o uso de tuplas

--- Estrturas de Dados Com Python/aula-02-tuples.py
+++ Estrturas de Dados Com Python/aula-02-tuples.py
@@ -12,3 +12,19 @@ print("Minha tupla:", minha_tupla)
 # Acessando elementos da tupla
 print("Primeiro elemento:", minha_tupla[0])  # Acessa o primeiro elemento
 print("Último elemento:", minha_tupla[-1])  # Acessa o último elemento
+
+# Matriz de tuplas
+matriz_tuplas = (
+    ("nome", "idade", "cidade"),
+    ("Alice", 30, "São Paulo"),
+    ("Bob", 25, "Rio de Janeiro"),
+    ("Charlie", 35, "Belo Horizonte")
+)
+
+print("Matriz de tuplas:", matriz_tuplas)
+
+# Desempacotamento de tuplas
+nome, idade, cidade = matriz_tuplas[1]  # Desempacota a segunda linha da matriz
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cidade:", cidade)
