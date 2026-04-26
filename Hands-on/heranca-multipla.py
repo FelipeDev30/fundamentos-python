ccb8b3ada44064e64f7dede7e2ec2620fb3ca3b9
= <=>
2026-04-26 19:00:56 -0300

    Estudando herança multipla e uso de kwargs para ajudar o interpretador Python referente aos argumentos passados da clsse PAI para filha.

---
+++ Hands-on/heranca-multipla.py
@@ -0,0 +1,32 @@
+""" 
+O que é herança múltipla?
+Herança múltipla é um conceito em programação orientada a objetos onde uma classe pode herdar características e comportamentos de mais de uma classe base. Isso permite que uma classe derive funcionalidades de várias classes, promovendo a reutilização de código e a flexibilidade na modelagem de objetos.
+
+Exemplo de herança múltipla em Python:
+
+"""
+class Animal:
+    def __init__(self, numero_patas):
+        self.numero_patas = numero_patas
+        
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}: { valor}' for chave, valor in self.__dict__.items()])}"
+
+class Mamifero(Animal):
+        def __init__(self, cor_pelo, **kwargs):
+            super().__init__(**kwargs)
+            self.cor_pelo = cor_pelo
+            
+class Ave(Animal):
+    def __init__(self, cor_bico, **kwargs):
+        super().__init__(**kwargs)
+        self.cor_bico = cor_bico
+
+class Felino(Mamifero):
+    pass
+
+class Ornitorrinco(Mamifero, Ave):
+    pass
+
+ornitorrinco = Ornitorrinco(numero_patas=4, cor_pelo='marrom', cor_bico='amarelo')
+print(ornitorrinco)
