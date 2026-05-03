e071970c6ee428cbd62c9fede10df17c479836b0
= <=>
2026-05-03 17:52:49 -0300

    Mais um exemplo de utilização de property

--- Programação Orientada a Objetos/Encapsulamento/aula03.py
+++ Programação Orientada a Objetos/Encapsulamento/aula03.py
@@ -1,4 +1,4 @@
-class Foo:
+""" class Foo:
     def __init__(self, x=None):
         self._x = x
     
@@ -19,4 +19,25 @@ print(foo.x)
 foo.x = 5
 print(foo.x)
 del foo.x
-print(foo.x)
+print(foo.x) """
+
+class Pessoa:
+    def __init__(self, nome, ano_nascimento):
+        nome = input("Digite o nome da pessoa: ")
+        ano_nascimento = int(input("Digite o ano de nascimento da pessoa: "))
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        ano_atual = int(input("Digite o ano atual: "))
+        return ano_atual - self._ano_nascimento
+
+if __name__ == "__main__":
+    pessoa = Pessoa(f"{Pessoa.nome}", Pessoa.idade)
+    print(f"Olá, meu nome é {pessoa.nome} e minha idade é {pessoa.idade}.")
+    
