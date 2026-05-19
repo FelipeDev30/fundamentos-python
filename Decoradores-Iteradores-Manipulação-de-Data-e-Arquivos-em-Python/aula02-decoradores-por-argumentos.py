cb09ee78043ef1a3299c00bef0654afdc9c4c9fb
= <=>
2026-05-19 19:27:59 -0300

    Aprendendo mais sobre decoradores e passagem de argumentos

--- Decoradores-Iteradores-Manipulação-de-Data-e-Arquivos-em-Python/aula02-decoradores-por-argumentos.py
+++ Decoradores-Iteradores-Manipulação-de-Data-e-Arquivos-em-Python/aula02-decoradores-por-argumentos.py
@@ -10,3 +10,17 @@ def saudacao(nome="", idade=0):
     print(f"Olá, {nome}! Você tem {idade} anos.")
 
 saudacao()
+
+def duplicar(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper
+
+@duplicar
+def aprender(tecnologia):
+    print(f"Estou aprendendo {tecnologia}!")
+    return tecnologia.upper()
+
+tecnologia = aprender("Python")
+print(tecnologia)
